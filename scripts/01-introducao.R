14df0d3751cf5711bf50289ab297478a123d6113
Sara Pamela <sarapamelaf@gmail.com>
2026-03-24 20:14:46 -0300

    atualiza projeto apos aula de 24/03

--- scripts/01-introducao.R
+++ scripts/01-introducao.R
@@ -2,8 +2,8 @@
 # DOCUMENTACAO DO SCRIPT
 # ============================================================
 # Arquivo: 01_introducao.R
-# Autor(a): <seu nome>
-# Data: <dd/mm/aaaa>
+# Autor(a): SARA PAMELA FERREIRA
+# Data: 24/03/2026
 # Objetivo: entender os fundamentos da linguagem R
 
 
@@ -17,9 +17,8 @@
 Sys.setlocale("LC_ALL", "pt_BR.UTF-8")
 
 
-# ==========================================
-# BLOCO 1 - R como uma grande calculadora
-# ==========================================
+#  R como uma grande calculadora ------------------------------------------
+
 
 # Carrega o pacote se ele tiver sido instalado
 library(tidyverse)
@@ -73,9 +72,9 @@ sqrt(225)
 round(3.14159, digits = 2)
 
 
-# ============================================================
-# BLOCO 2 - Tipos Atômicos e classes
-# ============================================================
+
+#  Tipos Atômicos e classes -----------------------------------------------
+
 
 # Os tipos de dados definem como os dados
 # são armazenados na memória.
@@ -113,9 +112,10 @@ f <- as.numeric(c)
 f
 
 
-# ============================================================
-# BLOCO 3 - Vetores numericos e vetorizacao
-# ============================================================
+
+# Vetores numericos e vetorizacao -----------------------------------------
+
+
 
 # Atalho de teclado para inserir <- :
 # Alt + - (Windows/Linux)
@@ -167,9 +167,9 @@ max(receita_diaria)
 ?length
 
 
-# ============================================================
-# BLOCO 4 - Vetores
-# ============================================================
+
+# Vetores -----------------------------------------------------------------
+
 
 # vetor de caracteres (strings) com nome da empresa
 nome_empresa <- c("Loja A", "Loja B", "Loja C")
@@ -186,10 +186,10 @@ meta_batida
 class(meta_batida)
 
 
-# ============================================================
-# BLOCO 5 - Criando uma tibble com dados ficticios de vendas
-# e primeiros exemplos com dplyr
-# ============================================================
+
+# BLOCO 5 - Criando uma tibble com dados ficticios de vendas e pr --------
+
+
 
 # Neste bloco vamos criar um pequeno conjunto de dados
 # semelhante a um banco de dados simples de vendas.
@@ -306,9 +306,9 @@ vendas_diarias |>
 vendas_diarias |>
   filter(between(receita, 10000, 11500))
 
-# ============================================================
-# BLOCO 6 - Manipulação de dados com dplyr e uso do pipe
-# ============================================================
+
+# Manipulação de dados com dplyr e uso do pipe ----------------------------
+
 
 # O operador pipe |> permite encadear operações
 # de forma mais legível.
@@ -323,25 +323,6 @@ vendas_diarias |>
 #   funcao1() |>
 #   funcao2() |>
 #   funcao3()
-
-
-# ------------------------------------------------------------
-# Boas práticas no RStudio
-# ------------------------------------------------------------
-
-# LEMBRAR DE MOSTRAR PARA OS ALUNOS:
-
-# 1) Configurar o RStudio para usar o pipe nativo |>
-
-# 2) Atalho de teclado para inserir pipe
-# Ctrl + Shift + M (Windows/Linux)
-
-# 3) Formatar código automaticamente
-# Code -> Reformat Selection
-
-# 4) (OPCIPONAL) Mostrar como reorganizar os painéis do RStudio
-# Console, Environment, Source, Files etc.
-
 
 # ------------------------------------------------------------
 # Exemplo 1 - selecionar e ordenar dados
@@ -448,7 +429,11 @@ ggplot(vendas_diarias, aes(x = data, y = lucro, size = receita, color = loja)) +
 
 
 # ============================================================
-# BLOCO 8 - Resolução dos exercícios propostos nos slides
+# BLOCO 8 - Resolução dos exercícios da lista 
+
+
+
+
 # ============================================================
 
 
