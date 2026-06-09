7c29c786520ece2ee104529b53d9566d406f40b0
Sara Pamela <sarapamelaf@gmail.com>
2026-06-09 19:44:30 -0300

    resolução avaliação 2

--- avaliacoes/02-avaliação/02-avaliacao-resolucao.R
+++ avaliacoes/02-avaliação/02-avaliacao-resolucao.R
@@ -1,8 +1,8 @@
 # Arquivo: 02-avaliacao-resolucao.R
-# Integrante 1: EVELYN C. M. LEAL
-# Integrante 2: ISADORA LEAL FARIA
-# Integrante 3: KEROLLAYNE BORGES
-# Integrante 4: SARA PAMELA FERREIRA
+# Integrante 1: Evellyn Leal
+# Integrante 2: Isadora Faria
+# Integrante 3: Kerollayne
+# Integrante 4: Sara Pamela 
 # Data: 09/06/2026
 # Objetivo: Resolução da Avaliação 2 — Introdução à Ciência de Dados
 
@@ -12,27 +12,32 @@ options(digits = 5, scipen = 999)
 
 # Exercício 1 -------------------------------------------------
 
-# a) 
-# Um possivel espaço amostral é: S = {renova, não renova}
-# Também seria aceitavel escrever:
-# S = {renova contrato, não renova contrato}
-# ou outra redação equivalente.
-
-
-# b) 
+# a) Um possível espaço amostral é: S = {renovou ou não renovou}
 #
 
-# c) 
+# b) Um evento associado à renovação do contrato. é: A = {renovação}
+# Em palavras: A é o evento "o cliente realiza a renovação".
 #
 
-# d) 
+# c) A probabilidade desse evento pode ser escrita como:
+# P(A)
+
+# d) Antes de definir uma variável aleatória, o resultado observado não é
+# diretamente numérico.
+# Ele é qualitativo/binário: renova ou não renova
+# Ele só passa a ser representado numericamente depois que definimos X.
+
+# e) Uma variável aleatória indicadora adequada é:
+# X = 1 se o cliente realiza a renovação;
+# X = 0 se o cliente não realiza a renovação
 #
+# Com essa definição, o evento A = {renovação} também pode ser escrito
+# como X = 1. Assim, P(A) = P(X = 1).
 
-# e) 
-#
-
-# f) 
-
+# f) A distribuição de probabilidade que mais se adequa para representar essa 
+# variavel é a Bernoulli, pois ela é usada para uma unica observação,
+# com dois resultados possíveis, sucesso ou fracasso
+# (No caso, renova ou não renova).
 
 
 # Exercício 2 -------------------------------------------------
@@ -68,39 +73,59 @@ options(digits = 5, scipen = 999)
 
 # Exercício 3 -------------------------------------------------
 
-# Digite e execute o código necessário e escreva a 
-# interpretação em comentários.
-
-
 set.seed(123)
 
-# item a)
+# a) 
 
 lambda <- 10
 capacidade <- 13
 n_simulacoes <- 1000
 
 
-# item b) Simula os valores de usando a função adequada do R
+# b) 
+
 clientes <- rpois(n = n_simulacoes, lambda = lambda)
 head(clientes)
 
 
-# item c) calcula a média simulada de clientes por hora de pico
+# c) 
+
 media_simulada <- mean(clientes)
 media_simulada
 
 
-# item d)  Calculo da proporção de valores 
-# simulados em que a capacidade de 13 clientes foi excedida.
+# d)
+
 prop_acima_capacidade <- mean(clientes > capacidade)
 prop_acima_capacidade
 
 
-# item e) Valor do percentil 95 da contagem de clientes por hora de pico.
+# e) 
+
 percentil_95 <- quantile(clientes, 0.95)
 percentil_95
 
 
+#f) 
 
+# A média simulada ficou coerente com a média do modelo?
+# R: A média simulada ficou perto de 10 clientes p/ hora, como esperado
+# no modelo (Poisson(lambda = 10).)  
 
+# Em que proporção das simulações a contagem de clientes 
+# ficou acima da capacidade atual de 13 clientes?
+# R: A capacidade de 13 clientes foi ultrapassada em cerca de 12,5% dos valores
+# simulados. Em uma hora de pico nesse modelo, a unidade ficaria 
+# acima da capacidade com frequência.    
+
+# O que significa o percentil 95 da contagem de clientes por hora de pico?
+# R: Nessa simulação o percentil 95 foi igual a 15, e isso indica
+# que uma capacidade de 15 clientes p/ hora cobriria cerca de 95% das simulações.
+
+# Esse percentil está próximo ou acima da capacidade atual?
+# R: Esse percentil está acima da capacidade atual, que no caso, seria 13.
+
+# Com base nesses resultados, a capacidade atual parece suficiente para a
+# maior parte das situações de pico, ou parece limitada?
+# R: Parece limitada, seria melhor, aumentar a capácidade para 15 clientes 
+# p/ hora, porque 15 atenderia aproximadamente 95% das situações simuladas.
